bac4c6bfc89cf5b785964d24ab303bc663dc5a6e
prajwaldsouza1 <prajwalrovin18@gmail.com>
2022-11-10 19:31:36 +0530

    commit

---
+++ mernstackprg/src/Home.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+// import {Link} from 'react-router-dom'
+// export default function Home() {
+//   return (
+//     <>
+//     <Link to='/'>Home</Link><br/>
+//     <Link to='/a1'>Module</Link><br/>
+//     <Link to='/a2'>Arraymethod</Link><br/>
+//     <Link to='/a3'>Arrow</Link><br/>
+//     <Link to='/a4'>Class</Link><br/>
+//     <Link to='/a5'>Classes</Link><br/>
+//     <Link to='/a6'>Conditional</Link><br/>
+//     <Link to='/a7'>Destructure</Link><br/>
+//     <Link to='/a8'>Events</Link><br/>
+//     <Link to='/a9'>Project1</Link><br/>
+//     <Link to='/a10'>List</Link><br/>
+      
+//     </>
+    
+//   )
+// }
---
+++ mernstackprg/src/reactrouter.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+// import {BrowserRouter,Routes,Route}from "react-router-dom"
+// import List from './list'
+// import Module from './Module'
+// import Arraymethod from './Arraymethod'
+// import Arrow from './arrow'
+// import Class from './class'
+// import Classes from './classes'
+// import Conditional from './conditional'
+// import  Destructure from './destructure'
+// import Events from './events'
+// import Project1 from './Project1'
+// import Home from './Home'
+// export default function reactrouter() {
+//   return (
+//     <div>
+//         <BrowserRouter>
+//         <Routes>
+
+//             <Route exact path='/' element={<Home/>}></Route>
+//             <Route exact path='/a1' element={<Module/>}></Route>
+//             <Route exact path='/a2' element={<Arraymethod/>}></Route>
+//             <Route exact path='/a3' element={<Arrow/>}></Route>
+//             <Route exact path='/a4' element={<Class/>}></Route>
+//             <Route exact path='/a5' element={<Classes/>}></Route>
+//             <Route exact path='/a6' element={<Conditional/>}></Route>
+//             <Route exact path='/a7' element={<Destructure/>}></Route>
+//             <Route exact path='/a8' element={<Events/>}></Route>
+//             <Route exact path='/a9' element={<Project1/>}></Route>
+//             <Route exact path='/a10' element={<List/>}></Route>
+//         </Routes>
+
+//         </BrowserRouter>      
+
+//     </div>
+//   )
+// }
